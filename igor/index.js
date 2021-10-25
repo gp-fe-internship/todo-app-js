9d23a9a1ae44508deaeb57917001ff4fe6c8b791
Fawkes <igrusu93@gmail.com>
2021-10-26 00:31:11 +0300

    initial commit

---
+++ igor/index.js
@@ -0,0 +1,75 @@
+const addButton = document.querySelector('#todo-add');
+const todoInputElement = document.querySelector('#todo-input');
+const todoListElement = document.querySelector('#todolist');
+const todoInputCountPlaceholder = document.querySelector('#input-count');
+const todoListTotalCount = document.querySelector('#total')
+
+todoInputElement.addEventListener('keyup', handleInputChange);
+addButton.addEventListener('click', handleAddButtonClick);
+
+
+function handleInputChange(event) {
+    handleInputCountUpdate(handleInputCount(event.target.value))
+}
+
+function handleInputCount(inputValue) {
+    return inputValue.length;
+}
+
+function handleInputCountUpdate(value) {
+
+    if (value === 0) {
+        todoInputCountPlaceholder.innerText = '';
+        return;
+    }
+
+    todoInputCountPlaceholder.innerText = `Characters count: ${value}`;
+}
+
+function handleAddButtonClick() {
+
+    const inputValue = todoInputElement.value;
+
+    if (!inputValue) {
+        return;
+    }
+
+    todoListElement.appendChild(createListItem(inputValue));
+    clearInput();
+    countTotalTasks();
+}
+
+function createListItem(value) {
+
+    if (!value) {
+        return;
+    }
+
+    const listItem = document.createElement('li');
+
+    const checkBoxElement = document.createElement('input');
+    checkBoxElement.type = 'checkbox';
+
+    const removeItemButton = document.createElement('button');
+    removeItemButton.innerText = 'X';
+
+    const spanElement = document.createElement('span');
+    spanElement.innerText = value;
+
+
+    listItem.classList.add('todolist__item');
+    listItem.appendChild(checkBoxElement);
+    listItem.appendChild(spanElement);
+    listItem.appendChild(removeItemButton);
+
+    return listItem;
+}
+
+function clearInput() {
+    todoInputElement.value = '';
+    todoInputCountPlaceholder.innerText = '';
+}
+
+function countTotalTasks() {
+    todoListTotalCount.innerText = `Total tasks: ${todoListElement.childElementCount}`
+}
