42211ab4d11110ad331b7b74fcc3eb7d7c5b531e
Dobrea Vladislav <vlad.d@goparrot.ai>
2021-10-26 17:33:01 +0300

    feat(listItem): handle list click actions

--- index.js
+++ index.js
@@ -2,63 +2,85 @@ const addButton = document.querySelector('#todo-add');
 const todoInputElement = document.querySelector('#todo-input');
 const todoListElement = document.querySelector('#todolist');
 const todoInputCountPlaceholder = document.querySelector('#input-count');
+const todoListTotalCount = document.querySelector('#total')
 
 todoInputElement.addEventListener('keyup', handleInputChange);
 addButton.addEventListener('click', handleAddButtonClick);
-
+todoListElement.addEventListener('click', handleListItemClick)
 
 function handleInputChange(event) {
-  handleInputCountUpdate(handleInputCount(event.target.value))
+    handleInputCountUpdate(handleInputCount(event.target.value))
 }
 
 function handleInputCount(inputValue) {
-  return inputValue.length;
+    return inputValue.length;
 }
 
 function handleInputCountUpdate(value) {
 
-  if(value === 0) {
-    todoInputCountPlaceholder.innerText = '';
-    return;
-  }
+    if (value === 0) {
+        todoInputCountPlaceholder.innerText = '';
+        return;
+    }
 
-  todoInputCountPlaceholder.innerText = `Characters count: ${value}`;
+    todoInputCountPlaceholder.innerText = `Characters count: ${value}`;
 }
 
-function handleAddButtonClick () {
+function handleAddButtonClick() {
 
-  const inputValue = todoInputElement.value;
-  
-  if (!inputValue) {
-    return;
-  }
+    const inputValue = todoInputElement.value;
 
-  todoListElement.appendChild(createListItem(inputValue)); 
+    if (!inputValue) {
+        return;
+    }
 
+    todoListElement.appendChild(createListItem(inputValue));
+    clearInput();
+    countTotalTasks();
 }
 
 function createListItem(value) {
 
-  if(!value) {
-    return;
+    if (!value) {
+        return;
+    }
+
+    const listItem = document.createElement('li');
+
+    const checkBoxElement = document.createElement('input');
+    checkBoxElement.type = 'checkbox';
+    checkBoxElement.setAttribute('data-action', 'complete')
+
+    const removeItemButton = document.createElement('button');
+    removeItemButton.innerText = 'X';
+    removeItemButton.setAttribute('data-action', 'remove');
+
+    const spanElement = document.createElement('span');
+    spanElement.innerText = value;
+
+
+    listItem.classList.add('todolist__item');
+    listItem.appendChild(checkBoxElement);
+    listItem.appendChild(spanElement);
+    listItem.appendChild(removeItemButton);
+
+    return listItem;
+}
+
+function clearInput() {
+    todoInputElement.value = '';
+    todoInputCountPlaceholder.innerText = '';
+}
+
+function countTotalTasks() {
+    todoListTotalCount.innerText = `Total tasks: ${todoListElement.childElementCount}`
+}
+
+function handleListItemClick(event) {
+  if (event.target.dataset.action === 'remove') {
+    event.target.closest('li').remove();
   }
-
-  const listItem = document.createElement('li');
-  
-  const checkBoxElement = document.createElement('input');
-  checkBoxElement.type = 'checkbox';
-  
-  const removeItemButton = document.createElement('button');
-  removeItemButton.innerText = 'X';
-
-  const spanElement = document.createElement('span');
-  spanElement.innerText = value;
-
-
-  listItem.classList.add('todolist__item');
-  listItem.appendChild(checkBoxElement);
-  listItem.appendChild(spanElement);
-  listItem.appendChild(removeItemButton);
-
-  return listItem;
+  if (event.target.dataset.action === 'complete') {
+    event.target.closest('li').classList.toggle('complete');
+  }
 }
