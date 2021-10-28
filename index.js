2c41bde09a680edb6b262c58704caf2d083de192
Dobrea Vladislav <vlad.d@goparrot.ai>
2021-10-28 11:30:03 +0300

    feat(storage): perssits todos in localStorage

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const TODO_LOCALSTORAGE_KEY = 'todos';
+
 const addButton = document.querySelector('#todo-add');
 const todoInputElement = document.querySelector('#todo-input');
 const todoListElement = document.querySelector('#todolist');
@@ -64,8 +66,53 @@ function createListItem(value) {
     listItem.appendChild(spanElement);
     listItem.appendChild(removeItemButton);
 
+    const todoId = crypto.randomUUID();
+    listItem.setAttribute('data-todo-id', todoId);
+
+
+    // LocalStorage logic
+    const todos = getTodosFromLocalStorage();
+    todos.push({
+        id: todoId,
+        text: value,
+        complete: false,
+    })
+    setTodosToLocalStorage(todos);
+
     return listItem;
 }
+
+
+function renderTodoItem(todo) {
+    const { id, text, complete } = todo;
+
+    const listItem = document.createElement('li');
+
+    const checkBoxElement = document.createElement('input');
+    checkBoxElement.type = 'checkbox';
+    checkBoxElement.setAttribute('data-action', 'complete')
+
+    const removeItemButton = document.createElement('button');
+    removeItemButton.innerText = 'X';
+    removeItemButton.setAttribute('data-action', 'remove');
+
+    const spanElement = document.createElement('span');
+    spanElement.innerText = text;
+
+
+    listItem.classList.add('todolist__item');
+    if (complete) {
+        listItem.classList.add('complete');
+    }
+    listItem.appendChild(checkBoxElement);
+    listItem.appendChild(spanElement);
+    listItem.appendChild(removeItemButton);
+
+    listItem.setAttribute('data-todo-id', id);
+    todoListElement.appendChild(listItem);
+}
+
+
 
 function clearInput() {
     todoInputElement.value = '';
@@ -84,3 +131,45 @@ function handleListItemClick(event) {
     event.target.closest('li').classList.toggle('complete');
   }
 }
+
+
+
+function setTodosToLocalStorage (todos = []) {
+    localStorage.setItem(TODO_LOCALSTORAGE_KEY, JSON.stringify(todos));
+    
+}
+
+function getTodosFromLocalStorage() {
+    return JSON.parse(localStorage.getItem(TODO_LOCALSTORAGE_KEY));
+}
+
+const todos = getTodosFromLocalStorage();
+
+if (!todos || !todos.length) {
+    console.log('empty storage')
+    setTodosToLocalStorage();
+} else {
+    console.log('render todos')
+    todos.map((todo) => renderTodoItem(todo));
+}
+
+/**
+ * 1. Page reload
+ *  - Check localstorage for todo's
+ *  - If there are no todos do nothing
+ *  - If there are todos, iterate and render items based on data
+ * 2. Create todo
+ *  - Read localstorage data (transform array)
+ *  - Get data for new item id
+ *  - Push data to array (todos)
+ *  - Save data to localStorage
+ * 3. Edit todo
+ *  - Read localStorage data (todos)
+ *  - Find in todos, edited element and update it
+ *  - Save data to localStorage (todos)
+ * 4. Remove todo
+ *  - Read localStorage data (todos)
+ *  - Filter out element from todos that matches remove item id
+ *  - Save data to localStorage (todos)
+ * 5. Fix bug with checkbox
+ */
